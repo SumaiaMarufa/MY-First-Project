e5283f8b0e36ec5d1efd0f80a4fa7a6cb275abc2
Sumaia Marufa <sumaia220520@gmail.com>
2024-08-17 09:50:29 +0600

    Method

---
+++ Method.java
@@ -0,0 +1,17 @@
+public class Method
+ {
+    static void Newvariable(char ch )
+{
+    System.out.println(ch+"All alphabets are");
+ }
+public static void main(String[] args)
+ {
+    for (char ch ='A'; ch <='Z' ; ch++) 
+        
+    
+    {
+        System.out.println(ch);    
+      }        
+}
+
+}
